7a5da47dcac020dddb947c51cdd9615f52143723
BonoDV <84352335+BonoDV@users.noreply.github.com>
2025-10-08 11:35:07 +0200

    fix: analytics doesn't work

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BoardComponent } from './components/board/board.component';
-import { Analytics } from '@vercel/analytics/next';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BoardComponent],
+  imports: [RouterOutlet, BoardComponent,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
